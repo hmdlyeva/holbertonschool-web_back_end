05b8afabb66ffb73d09925e835ea8a615b4b2a0b
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-07-21 18:51:15 +0400

    taskk

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -6,7 +6,7 @@ export default class Building {
       this.constructor !== Building && this.evacuationWarningMessage === undefined
     ) {
       throw Error(
-        'Class extending Building must override evacuationWarningMessage'
+        'Class extending Building must override evacuationWarningMessage',
       );
     }
   }
