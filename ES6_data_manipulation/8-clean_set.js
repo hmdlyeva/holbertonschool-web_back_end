ede327130096673265e021e0aab146559d4e3c57
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-07-28 15:47:54 +0400

    data m

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
-  if (!set && !startString && !(set instanceof Set) &&
-    typeof startString !== 'string'
+  if (!set && !startString && !(set instanceof Set)
+    && typeof startString !== 'string'
   ) {
     return '';
   }
