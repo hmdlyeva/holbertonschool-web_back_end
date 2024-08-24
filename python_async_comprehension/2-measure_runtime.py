3a937961fefaa465b168adc55cf11631cad4b6e6
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-08-24 11:13:31 +0400

    pycode

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -2,7 +2,7 @@
 """async measure runtime"""
 import time
 import asyncio
-async_comprehension = __import__('0-async_comprehension').async_comprehension
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
