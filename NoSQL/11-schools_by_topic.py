424fb62fed7f411b60b22ff99f46ec945828deab
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-08-24 21:50:02 +0400

    dbs

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""returns the list of school having a specific topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """returns specific topic"""
+    doc_for_topic = mongo_collection.find({"topic":topic})
+    return doc_for_topic
