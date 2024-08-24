edd3b0acd305f417f8eaa8832af257f69e0ff6c5
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-08-24 10:59:32 +0400

    pycode

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,6 +3,7 @@
 
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension():
     """collect 10 random numbers then return"""
     return [_ async for _ in async_generator()]
