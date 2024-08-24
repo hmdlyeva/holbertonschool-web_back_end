70254e1cc83d9949aacae65cf1fdb7499bf6e266
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-08-24 11:38:57 +0400

    pycode

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,7 +3,7 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int,int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """index range"""
     index = page * page_size - page_size
     index_1 = index + page_size
