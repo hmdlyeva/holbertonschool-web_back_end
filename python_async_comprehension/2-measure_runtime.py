2914edb329f5301beb4b091a179116b5deec2923
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-08-24 11:14:38 +0400

    pycode

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """measure runtime"""
-    tasks = [async_comprehension for _ in range(10)]
+    tasks = [async_comprehension() for _ in range(10)]
     start_time = time.perf_counter()
     await asyncio.gather(*tasks)
     end_time = time.perf_counter()
